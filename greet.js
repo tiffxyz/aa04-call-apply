a583838f226b5d22895a397a0e6217c888786287
abraham <agarciasantos14@gmail.com>
2024-03-11 21:47:21 -0700

    complete

--- greet.js
+++ greet.js
@@ -1,10 +1,13 @@
 // DO NOT MODIFY THE say FUNCTION
 function say(name) {
   console.log(`${this.message} ${name}!`);
+
+
 }
 
 helloMessage = { message: 'Hello,' };
 heyThereMessage = { message: 'Hey there,' };
 
-say(helloMessage, "John");
-say(heyThereMessage, "Michael");
+
+say.call(helloMessage, "John");
+say.call(heyThereMessage, "Michael");
